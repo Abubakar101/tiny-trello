1aa4fc301a5e3e8dab6984ea1f04315adb6c6f7d
Muhammad Abubakar <muhammadabubakar0419@gmail.com>
2018-02-14 14:03:50 -0500

    delete unessary routes

--- mirage/config.js
+++ mirage/config.js
@@ -1,18 +1,14 @@
 export default function() {
-  this.namespace = '/api';
+  this.namespace = "/api";
 
-  this.get('/lists');
-  this.post('/lists');
-  this.get('/lists/:id');
-  this.put('/lists/:id');
-  this.patch('/items/:id');
-  this.del('/lists/:id');
+  this.get("/lists");
+  this.post("/lists");
+  this.get("/lists/:id");
 
-  this.get('/items');
-  this.post('/items');
-  this.get('/items/:id');
-  this.put('/items/:id');
-  this.patch('/items/:id');
-  this.del('/items/:id');
-
+  this.get("/items");
+  this.post("/items");
+  this.get("/items/:id");
+  this.put("/items/:id");
+  this.patch("/items/:id");
+  this.del("/items/:id");
 }
